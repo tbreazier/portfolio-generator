66ae672196ed5c25ec93ae0f552abb32550493dd
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-07-15 20:38:41 -0500

    first question

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const [name, github] = profileDataArgs
 
-fs.writeFile("index.html", generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile("index.html", generatePage(name, github), err => {
+//     if (err) throw err;
 
-    console.log("Portfolio complete! Checkout index.html to see the output!");
-});
+//     console.log("Portfolio complete! Checkout index.html to see the output!");
+// });
+
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
