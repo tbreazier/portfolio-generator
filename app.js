766c67747dacd0f7e3b2e8d3e882bf1e72b7da4e
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-07-17 21:27:39 -0500

    final site

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-const fs = require("fs");
+const {writeFile, copyFile} = require('./utils/generate-site.js');
 const generatePage = require("./src/page-template.js");
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-// const [name, github] = profileDataArgs
-
-// fs.writeFile("index.html", generatePage(name, github), err => {
-//     if (err) throw err;
-
-//     console.log("Portfolio complete! Checkout index.html to see the output!");
-// });
-
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -137,11 +128,18 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
-});
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
